eef07bf314a3ed897103175613119fdc346e898c
Mihir <mihirsharma60@gmail.com>
2023-04-23 10:31:09 +0530

    React with vite

---
+++ react-vite/vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        {/* <a href="https://vitejs.dev" target="_blank" r>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+      </div>
+      <h1>Mihir</h1>
+      
+    </>
+  )
+}
+
+export default App
